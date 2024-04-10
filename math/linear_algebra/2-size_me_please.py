9521b6c628206daa24599554046d0388f5b5def6
Mateo Zouko <mateozouko1@gmail.com>
2024-04-10 16:49:27 +0000

    task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+"""
+Task 2
+"""
 def matrix_shape(matrix):
+    """
+    Function that returns the shape of a matrix
+    """
     matrix_size = []
 
     if len(matrix) > 1:
         matrix_size.append(len(matrix))
         if isinstance(matrix[0], list):
             matrix_size.append(len(matrix[0]))
-        if isinstance(matrix[0][0], list):
-            matrix_size.append(len(matrix[0][0]))
+            if isinstance(matrix[0][0], list):
+                matrix_size.append(len(matrix[0][0]))
+    elif len(matrix) == 1:
+        matrix_size.append(len(matrix))
     return matrix_size
