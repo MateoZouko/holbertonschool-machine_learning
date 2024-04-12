3a90796f7f7fa7c4d3046f2421d20ef4de88fff6
Mateo Zouko <mateozouko1@gmail.com>
2024-04-12 14:57:53 +0000

    fixing

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,6 +8,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Function that concatenates two matrices along a specific axis
     """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
 
     if axis == 0:
         return [row.copy() for row in mat1] + [row.copy() for row in mat2]
