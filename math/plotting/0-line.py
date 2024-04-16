0679c6e42d555cf7de7034008e3fce3364e5d7ab
Mateo Zouko <mateozouko1@gmail.com>
2024-04-16 11:19:41 -0300

    task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -15,4 +15,6 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y, color='red')
+    x = np.arange(0, 11)
+    plt.plot(x, y, color='red')
+    plt.xlim([0, 10])
