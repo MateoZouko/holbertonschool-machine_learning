305f35ccde083266c6ba320b0329917234d2710c
Mateo Zouko <mateozouko1@gmail.com>
2024-04-25 22:03:50 -0300

    fixing

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -54,9 +54,9 @@ class Node:
         node_repr = f"{node_type} [feature={self.feature},\
  threshold={self.threshold}]\n"
         if self.left_child:
-            node_repr += self.left_child_add_prefix(self.left_child.__str__())
+            node_repr += "\n" + self.left_child_add_prefix(self.left_child.__str__())
         if self.right_child:
-            node_repr += self.right_child_add_prefix(self.right_child.__str__())
+            node_repr += "\n" + self.right_child_add_prefix(self.right_child.__str__())
         return node_repr
 
     def left_child_add_prefix(self, text):
