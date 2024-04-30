643aff8a688de48a5f68024f6d16f5b708104fe9
Mateo Zouko <mateozouko1@gmail.com>
2024-04-30 11:12:59 -0300

    task 0

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Task 0
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """
+    Class Neuron
+    """
+
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
