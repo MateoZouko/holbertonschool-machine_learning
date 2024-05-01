425f58a9f19bb526678ac530b8f5e620de94b6a2
Mateo Zouko <mateozouko1@gmail.com>
2024-05-01 11:29:19 -0300

    task 4

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Task 1
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """
+    Class Neuron
+    """
+
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        Getter for W
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        Getter for b
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        Getter for A
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        """
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        """
+        m = Y.shape[1]
+        return -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neuron predictions
+        """
+        A = self.forward_prop(X)
+        return np.round(A).astype(int), self.cost(Y, A)
