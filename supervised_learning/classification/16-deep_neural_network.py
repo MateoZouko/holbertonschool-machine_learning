b6124f6bb0b0a5a379242769bebb8c7284ed5799
Mateo Zouko <mateozouko1@gmail.com>
2024-05-06 20:21:53 -0300

    task 16

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Task 16
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class DeepNeuralNetwork:
+    """
+    Deep neural network
+    """
+
+    def __init__(self, nx, layers):
+        """
+        class constructor
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(n, int) and n > 0 for n in layers):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(len(layers)):
+            if type(layers[i]) is not int or layers[i] < 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            if i == 0:
+                self.weights['W1'] = np.random.randn(
+                        layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights['W{}'.format(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            self.weights['b{}'.format(i + 1)] = np.zeros((layers[i], 1))
