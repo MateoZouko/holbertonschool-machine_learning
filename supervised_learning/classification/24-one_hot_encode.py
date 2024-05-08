8f78fb9d0ef05aeaa53bccbe13a40722a7432afd
Mateo Zouko <mateozouko1@gmail.com>
2024-05-08 14:19:11 -0300

    task 24

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+    converts a numeric label vector into a one-hot matrix
+    """
+    if type(Y) is not np.ndarray or len(Y) == 0:
+        return None
+    if type(classes) is not int or classes <= np.amax(Y):
+        return None
+    one_hot = np.zeros((classes, Y.shape[0]))
+    one_hot[Y, np.arange(Y.shape[0])] = 1
+    return one_hot
