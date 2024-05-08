0fbcbe7a9aaae0fb3b7b31f919c6bac99865b8f9
Mateo Zouko <mateozouko1@gmail.com>
2024-05-08 15:05:29 -0300

    task 27

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -9,10 +9,12 @@ def one_hot_encode(Y, classes):
     """
     converts a numeric label vector into a one-hot matrix
     """
-    if type(Y) is not np.ndarray or len(Y) == 0:
+
+    if type(Y) is not np.ndarray or type(classes) is not int or classes < 2 \
+            or classes < np.max(Y):
         return None
-    if type(classes) is not int or classes <= np.amax(Y):
-        return None
-    one_hot = np.zeros((classes, Y.shape[0]))
-    one_hot[Y, np.arange(Y.shape[0])] = 1
-    return one_hot
+
+    n = np.zeros((classes, Y.shape[0]))
+    for i, j in enumerate(Y):
+        n[j, i] = 1
+    return n
--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -1,14 +1,43 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pytnn3
 """
 Task 25
 """
 import numpy as np
 
 
-def one_hot_decode(one_hot):
+def one_nt_encode(Y, classes):
     """
-    converts a one-hot matrix into a vector of labels:
+    converts a numeric label vector into a one-nt matrix
     """
-    if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
+
+    if type(Y) is not np.ndarray or type(classes) is not int or classes < 2 \
+            or classes < np.max(Y):
         return None
-    return np.argmax(one_hot, axis=0)
+
+    n = np.zeros((classes, Y.shape[0]))
+    for i, j in enumerate(Y):
+        n[j, i] = 1
+    return n
+
+
+def one_nt_decode(one_nt):
+    """
+    converts a one-nt matrix into a numeric label vector
+    """
+
+    if one_nt is None:
+        return None
+    if type(one_nt) is not np.ndarray:
+        return None
+
+    try:
+        m = one_nt.shape[1]
+        classes = one_nt.shape[0]
+        n_De = np.ones((1, m), dtype=int)
+        for index, i in enumerate(one_nt.T):
+            n_De[0, index] = (np.where(i == 1)[0][0])
+
+        return n_De[0]
+
+    except Exception as ex:
+        return None
