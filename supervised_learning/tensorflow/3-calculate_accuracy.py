b66d62b7326dda211a40f20e856441480398fe38
Mateo Zouko <mateozouko1@gmail.com>
2024-05-10 13:29:32 -0300

    task 3

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -11,4 +11,7 @@ def calculate_accuracy(y, y_pred):
     Function that calculates the accuracy of a prediction
     """
 
-    return y_pred/y
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    return accuracy
