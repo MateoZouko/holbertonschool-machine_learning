5001cd1bfe264dd7739d060e5a4e92a9ba6938fe
Mateo Zouko <mateozouko1@gmail.com>
2024-05-10 16:10:45 -0300

    task 7

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+6th task on tensoflow project
+"""
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    X is a numpy.ndarray containing the input data to evaluate
+    """
+    saver = tf.train.import_meta_graph(save_path + ".meta")
+    with tf.Session() as sess:
+        saver.restore(sess, save_path)
+
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+
+        pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+
+        prediction_result, accuracy_result, loss_result = sess.run(
+            [pred, accuracy, loss], feed_dict={x: X, y: Y})
+
+        return prediction_result, accuracy_result, loss_result
