87fea80bcce092a1608e7dee92d74da8870e278a
Mateo Zouko <mateozouko1@gmail.com>
2024-05-10 16:17:12 -0300

    task 7

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -12,7 +12,6 @@ def evaluate(X, Y, save_path):
     saver = tf.train.import_meta_graph(save_path + '.meta')
     with tf.Session() as sess:
         saver.restore(sess, save_path)
-        print("Modelo cargado desde:", save_path)
 
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
@@ -23,9 +22,5 @@ def evaluate(X, Y, save_path):
 
         prediction_result, accuracy_result, loss_result = sess.run(
             [pred, accuracy, loss], feed_dict={x: X, y: Y})
-
-        print("Predicciones:", prediction_result)
-        print("Precisión:", accuracy_result)
-        print("Pérdida:", loss_result)
 
         return prediction_result, accuracy_result, loss_result
