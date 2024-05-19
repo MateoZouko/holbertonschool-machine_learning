d9df2d686d7f2cd634c30fd1a30ecab87e4d665c
Mateo Zouko <mateozouko1@gmail.com>
2024-05-19 10:46:40 -0300

    task 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task 6
+"""
+
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    sets up the gradient descent with momentum
+    optimization algorithm in TensorFlow
+    """
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
+    return optimizer
