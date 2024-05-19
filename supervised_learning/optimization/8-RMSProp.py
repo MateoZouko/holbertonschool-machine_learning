7a34d9707fca179937d866e9a27cf8d906caa9f6
Mateo Zouko <mateozouko1@gmail.com>
2024-05-19 11:04:25 -0300

    task 8

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task 8
+"""
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    sets up the RMSProp optimization algorithm in TensorFlow
+    """
+
+    optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha,
+                                            rho=beta2, epsilon=epsilon)
+    return optimizer
