854aba77ec2e2eff0f5f5cc40d705a88647fc33d
Mateo Zouko <mateozouko1@gmail.com>
2024-06-02 19:20:53 -0300

    task 0

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Task 1
+"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Task 1
+    """
+    model = K.Sequential()
+    model.add(
+        K.layers.Dense(
+            layers[0], input_shape=(nx,),
+            activation=activations[0],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+        )
+    )
+    for i in range(1, len(layers)):
+        model.add(K.layers.Dropout(1 - keep_prob))
+        model.add(
+            K.layers.Dense(
+                layers[i], activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha)
+            )
+        )
+    return model
