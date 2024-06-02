8f4da77804a486889bddb482573ee0790a810225
Mateo Zouko <mateozouko1@gmail.com>
2024-06-02 19:28:31 -0300

    task 3

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Task 2
+"""
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Converts a label vector into a one-hot matrix
+    """
+
+    one_hot_matrix = K.utils.to_categorical(labels, num_classes=classes)
+    return one_hot_matrix
