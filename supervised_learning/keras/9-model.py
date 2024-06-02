a24c58c91ff67c1a5d7e88391003dfae77a25cb2
Mateo Zouko <mateozouko1@gmail.com>
2024-06-02 20:03:48 -0300

    task 9

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Model Saving and Loading
+"""
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    Saves an entire model
+    """
+    network.save(filename)
+
+
+def load_model(filename):
+    """
+    Loads an entire model
+    """
+    return K.models.load_model(filename)
