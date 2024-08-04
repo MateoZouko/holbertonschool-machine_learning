b49b85dd49025cf4ac3c65291ceb1b2831d40220
Mateo Zouko <mateozouko@192.168.1.10>
2024-08-03 21:14:02 -0300

    task 3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 3"""
+
+
+def matrix_transpose(matrix):
+    """transposes a 2d matrix"""
+    mTransposed = [[] for x in range(len(matrix[0]))]
+    for x, arr in enumerate(mTransposed):
+        for row in matrix.copy():
+            arr.append(row[x])
+    return mTransposed
