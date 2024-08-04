16a24dc15cfacbef7e9cc40094d598d076342045
Mateo Zouko <mateozouko@192.168.1.10>
2024-08-03 21:18:45 -0300

    task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Task 7"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """cancats 2D matrices"""
+    if axis > 1 or axis < 0:
+        return None
+    matrix = []
+    for row in mat1:
+        matrix.append(row.copy())
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        for row in mat2:
+            matrix.append(row.copy())
+        return matrix
+    elif len(mat1) != len(mat2):
+        return None
+    for x, row in enumerate(matrix):
+        for item in mat2[x].copy():
+            row.append(item)
+    return matrix
