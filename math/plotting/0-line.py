6842857bf8e32824eef48eda3c3ef83621b3d825
Mateo Zouko <mateozouko@192.168.1.10>
2024-08-07 21:13:19 -0300

    task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,11 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """
+    plot y as a line graph
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     x = np.arange(0, 11)
     plt.plot(x, y, color='red')
     plt.xlim(0, 10)
-
