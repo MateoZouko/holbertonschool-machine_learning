8aaf9b889af482efae1ae2a5a54c1543a0d5668f
Mateo Zouko <mateozouko@192.168.1.7>
2024-08-23 21:23:52 -0300

    neuron tasks

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Task 3
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    class that represents a single neuron performing binary classification
+    """
+
+    def __init__(self, nx):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        getter W
+        """
+        return (self.__W)
+
+    @property
+    def b(self):
+        """
+        getter b
+        """
+        return (self.__b)
+
+    @property
+    def A(self):
+        """
+        getter A
+        """
+        return (self.__A)
+
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neuron
+
+        """
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return (self.A)
+
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using logistic regression
+        """
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
