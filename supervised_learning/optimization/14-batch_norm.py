12d802a19938487417dd1ea35fa2adabe6a8930e
Mateo Zouko <mateozouko1@gmail.com>
2024-09-15 18:45:54 -0300

    task 14

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -8,15 +8,28 @@ import tensorflow as tf
 
 def create_batch_norm_layer(prev, n, activation):
     """
-    creates a batch normalization layer for a neural network in tensorflow
+    Creates a batch normalization layer
+    for a neural network in TensorFlow.
     """
-    init = tf.keras.layers.Dense(n, 
-        kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg'))(prev)
-    batchN = tf.keras.layers.BatchNormalization(
-        epsilon=1e-7,
-        center=True,
-        scale=True,
-        beta_initializer='zeros',
-        gamma_initializer='ones', )(init)
-    activation = tf.keras.layers.Activation(activation)(batchN)
-    return activation
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    dense_layer = tf.keras.layers.Dense(units=n,
+                                        kernel_initializer=initializer)(prev)
+
+    gamma = tf.Variable(initial_value=tf.ones((n,),
+                                              dtype=tf.float32),
+                        trainable=True, name='gamma')
+    beta = tf.Variable(initial_value=tf.zeros((n,),
+                                              dtype=tf.float32),
+                       trainable=True, name='beta')
+
+    mean, variance = tf.nn.moments(dense_layer, axes=[0])
+
+    epsilon = 1e-7
+    batch_norm = tf.nn.batch_normalization(
+        dense_layer, mean, variance, beta, gamma, epsilon
+    )
+
+    if activation is not None:
+        return activation(batch_norm)
+    else:
+        return batch_norm
