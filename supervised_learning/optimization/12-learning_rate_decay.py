6dc64aab9f2662c45231153366f79f8d3daea5aa
Mateo Zouko <mateozouko1@gmail.com>
2024-09-15 18:14:02 -0300

    task 12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -5,15 +5,15 @@ Task 11
 
 import tensorflow as tf
 
-
 def learning_rate_decay(alpha, decay_rate, decay_step):
     """
-    function that creates a learning rate decay operation in tensorflow using
-    inverse time decay
+    Creates a learning rate decay operation in TensorFlow using inverse time decay.
     """
-
-    op = tf.train.inverse_time_decay(alpha, global_step=0,
-                                     decay_steps=decay_step,
-                                     decay_rate=decay_rate, staircase=True)
-
-    return op
+    lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True,
+        name='InverseTimeDecay'
+    )
+    return lr_schedule
