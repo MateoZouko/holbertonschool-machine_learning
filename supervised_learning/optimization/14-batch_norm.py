c59d6aa86b4a7ce6bd1d49c9375d64635b4e92c9
Mateo Zouko <mateozouko1@gmail.com>
2024-09-15 18:40:06 -0300

    task 14

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -10,11 +10,13 @@ def create_batch_norm_layer(prev, n, activation):
     """
     creates a batch normalization layer for a neural network in tensorflow
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, kernel_initializer=init)
-    Z = layer(prev)
-    gamma = tf.Variable(tf.constant(1.0, shape=[n]), name='gamma')
-    beta = tf.Variable(tf.constant(0.0, shape=[n]), name='beta')
-    mean, variance = tf.nn.moments(Z, axes=0)
-    Z_norm = tf.nn.batch_normalization(Z, mean, variance, beta, gamma, 1e-8)
-    return activation(Z_norm)
+    init = tf.keras.layers.Dense(n, 
+        kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg'))(prev)
+    batchN = tf.keras.layers.BatchNormalization(
+        epsilon=1e-7,
+        center=True,
+        scale=True,
+        beta_initializer='zeros',
+        gamma_initializer='ones', )(init)
+    activ = tf.keras.layers.Activation(activ)(batchN)
+    return activ
