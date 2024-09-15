323c2b841d3a6a76a42888bab6f3dc6309cced0c
Mateo Zouko <mateozouko@192.168.1.9>
2024-09-15 16:59:40 -0300

    task 3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -3,25 +3,18 @@
 Task 3
 """
 
-import numpy as np
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def create_mini_batches(X, Y, batch_size):
     """
-    function that creates mini-batches from a dataset
+    function that creates mini-batches from a data set
     """
     m = X.shape[0]
     mini_batches = []
-    shuffler = np.random.permutation(m)
-    X_shuffled = X[shuffler]
-    Y_shuffled = Y[shuffler]
-    num_batches = m // batch_size
-    for i in range(num_batches):
-        X_mini = X_shuffled[i * batch_size:(i + 1) * batch_size]
-        Y_mini = Y_shuffled[i * batch_size:(i + 1) * batch_size]
-        mini_batches.append((X_mini, Y_mini))
-    if m % batch_size != 0:
-        X_mini = X_shuffled[num_batches * batch_size:]
-        Y_mini = Y_shuffled[num_batches * batch_size:]
+    X_shuffled, Y_shuffled = shuffle_data(X, Y)
+    for i in range(0, m, batch_size):
+        X_mini = X_shuffled[i:i + batch_size]
+        Y_mini = Y_shuffled[i:i + batch_size]
         mini_batches.append((X_mini, Y_mini))
     return mini_batches
