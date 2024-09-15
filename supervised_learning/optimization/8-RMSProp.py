3e28369c5d87c095ffc2bf0a5643a9d839524670
Mateo Zouko <mateozouko1@gmail.com>
2024-09-15 17:45:55 -0300

    task 8

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Task 8
+"""
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    function that creates the training operation for a neural network in
+    tensorflow using the RMSProp optimization algorithm
+    """
+
+    optimizer = tf.keras.optimizers.RMSprop(alpha, beta2, epsilon)
+
+    return optimizer.minimize(loss)
