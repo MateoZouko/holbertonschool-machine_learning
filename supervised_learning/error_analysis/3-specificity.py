2430b62c46e57c4a9405236d5795e2134f46aafb
Mateo Zouko <mateozouko1@gmail.com>
2024-09-21 19:18:20 -0300

    task 3

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -11,4 +11,18 @@ def specificity(confusion):
     Function that calculates the specificity
     for each class in a confusion matrix
     """
-    return np.diagonal(confusion) / np.sum(confusion, axis=1)
+    classes = confusion.shape[0]
+    specificity_scores = np.zeros(classes)
+
+    for i in range(classes):
+        true_negatives = np.sum(confusion) - (np.sum
+                                              (confusion[i, :]) + np.sum(
+                                                 confusion[:, i]) -
+                                              confusion[i, i])
+
+        false_positives = np.sum(confusion[:, i]) - confusion[i, i]
+
+        specificity_scores[i] = true_negatives / (true_negatives
+                                                  + false_positives)
+
+    return specificity_scores
