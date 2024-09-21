8b9ea8e70de02b0b85bd3ad4541a035d6c35576a
Mateo Zouko <mateozouko1@gmail.com>
2024-09-21 19:09:04 -0300

    task 0

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Task 0
+"""
+
+import numpy as np
+
+def create_confusion_matrix(labels, logits):
+    """
+    Function that creates a confusion matrix
+    """
+    return np.dot(labels.T, logits)
