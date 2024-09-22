9b0302ce932e24e635016dbbec7034e0f57a2d0d
Mateo Zouko <mateozouko1@gmail.com>
2024-09-22 19:41:31 -0300

    task 2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -5,8 +5,10 @@ Task 1
 
 import tensorflow as tf
 
+
 def l2_reg_cost(cost, model):
     """
-    function that calculates the cost of a neural network with L2 regularization
+    function that calculates the cost of a
+    neural network with L2 regularization
     """
     return cost + model.losses
