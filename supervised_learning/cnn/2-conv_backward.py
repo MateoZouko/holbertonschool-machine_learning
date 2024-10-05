00a8c5b36406e6aade161c4f37c30f7456805f09
Mateo Zouko <mateozouko1@gmail.com>
2024-10-04 21:00:34 -0300

    task 2

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -6,9 +6,11 @@ Task 2
 import numpy as np
 
 
-def conv_backward(dZ, A_prev, W, b, activation='relu', padding="same", stride=(1, 1)):
+def conv_backward(dZ, A_prev, W, b, activation='relu',
+                  padding="same", stride=(1, 1)):
     """
-    Function that performs back propagation over a convolutional layer of a
+    Function that performs back propagation
+    over a convolutional layer of a
     neural network
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
@@ -28,22 +30,32 @@ def conv_backward(dZ, A_prev, W, b, activation='relu', padding="same", stride=(1
     A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                         'constant', constant_values=0)
 
-    dA_prev = np.zeros(A_prev_pad.shape)
-    dW = np.zeros(W.shape)
-    db = np.zeros(b.shape)
+    dA_prev = np.zeros_like(A_prev_pad)
+    dW = np.zeros_like(W)
+    db = np.zeros((1, 1, 1, c_new))
 
     for i in range(m):
         for j in range(h_new):
             for k in range(w_new):
                 for len in range(c_new):
-                    dA_prev[i, j * sh:j * sh + kh, k * sw:k * sw + kw, :] += \
-                        W[:, :, :, len] * dZ[i, j, k, len]
-                    dW[:, :, :, len] += A_prev_pad[i, j * sh:j * sh + kh,
-                                                   k * sw:k * sw +
-                                                   kw, :] * dZ[i, j, k, len]
+                    vert_start = j * sh
+                    vert_end = vert_start + kh
+                    horiz_start = k * sw
+                    horiz_end = horiz_start + kw
+
+                    A_slice = A_prev_pad[i, vert_start:vert_end,
+                                         horiz_start:horiz_end, :]
+
+                    dA_prev[i, vert_start:vert_end,
+                            horiz_start:horiz_end, :] += \
+                        W[:, :, :, ] * dZ[i, j, k, len]
+                    dW[:, :, :, len] += A_slice * dZ[i, j, k, len]
                     db[:, :, :, len] += dZ[i, j, k, len]
 
+    if padding == 'same':
+        dA_prev = dA_prev[:, ph:-ph, pw:-pw, :]
+
     if activation == 'relu':
-        dA_prev = np.where(A_prev_pad <= 0, 0, dA_prev)
+        dA_prev = np.where(A_prev_pad > 0, dA_prev, 0)
 
     return dA_prev, dW, db
