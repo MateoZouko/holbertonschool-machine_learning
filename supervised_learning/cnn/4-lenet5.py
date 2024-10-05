afb491d1dd7f4285843044b62e2c8cabbc8a6141
Mateo Zouko <mateozouko1@gmail.com>
2024-10-04 21:08:45 -0300

    task 4 w tf

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -4,7 +4,7 @@ Task 4
 """
 
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def lenet5(X, Y):
@@ -12,35 +12,31 @@ def lenet5(X, Y):
     Function that builds a modified version of the LeNet-5 architecture using
     tensorflow
     """
+    init = tf.contrib.layers.variance_scaling_initializer()
+    activation = tf.nn.relu
 
-    tf.keras.backend.clear_session()
+    conv1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
+                             padding='same', activation=activation,
+                             kernel_initializer=init)(X)
+    pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
 
-    model = tf.keras.Sequential()
+    conv2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                             padding='valid', activation=activation,
+                             kernel_initializer=init)(pool1)
+    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
 
-    model.add(tf.keras.layers.Conv2D(6, kernel_size=(5, 5), padding='same',
-                                     activation='relu',
-                                     input_shape=X.shape[1:]))
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),
-                                           strides=(2, 2)))
+    flatten = tf.layers.Flatten()(pool2)
 
-    model.add(tf.keras.layers.Conv2D(16, kernel_size=(5, 5),
-                                     activation='relu'))
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2),
-                                           strides=(2, 2)))
+    fc1 = tf.layers.Dense(units=120, activation=activation,
+                          kernel_initializer=init)(flatten)
+    fc2 = tf.layers.Dense(units=84, activation=activation,
+                          kernel_initializer=init)(fc1)
+    output = tf.layers.Dense(units=10, kernel_initializer=init)(fc2)
 
-    model.add(tf.keras.layers.Flatten())
+    loss = tf.losses.softmax_cross_entropy(Y, output)
+    train_op = tf.train.AdamOptimizer().minimize(loss)
 
-    model.add(tf.keras.layers.Dense(120, activation='relu'))
+    y_pred = tf.nn.softmax(output)
+    accuracy = tf.metrics.accuracy(Y, y_pred)
 
-    model.add(tf.keras.layers.Dense(84, activation='relu'))
-
-    model.add(tf.keras.layers.Dense(10, activation='softmax'))
-
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    history = model.fit(X, Y, batch_size=32, epochs=10,
-                        verbose=1, validation_data=(X, Y))
-
-    return model, history
+    return y_pred, train_op, loss, accuracy
