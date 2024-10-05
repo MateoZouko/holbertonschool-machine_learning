c59dca791324a60822e68e16f55bc1d3c8afdf63
Mateo Zouko <mateozouko1@gmail.com>
2024-10-04 21:06:15 -0300

    task 2 reformated

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -6,56 +6,56 @@ Task 2
 import numpy as np
 
 
-def conv_backward(dZ, A_prev, W, b, activation='relu',
-                  padding="same", stride=(1, 1)):
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """
-    Function that performs back propagation
-    over a convolutional layer of a
-    neural network
+    Performs backpropagation over a
+    convolutional layer of a neural network.
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
 
     if padding == 'valid':
-        ph = 0
-        pw = 0
+        ph, pw = 0, 0
     elif padding == 'same':
         ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
         pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
 
-    h_new = int((h_prev + 2 * ph - kh) / sh + 1)
-    w_new = int((w_prev + 2 * pw - kw) / sw + 1)
+    h_new = dZ.shape[1]
+    w_new = dZ.shape[2]
 
-    A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        'constant', constant_values=0)
-
-    dA_prev = np.zeros_like(A_prev_pad)
+    dA_prev = np.zeros_like(A_prev)
     dW = np.zeros_like(W)
-    db = np.zeros((1, 1, 1, c_new))
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+
+    A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph),
+                                 (pw, pw), (0, 0)),
+                        mode='constant', constant_values=0)
+    dA_prev_pad = np.pad(dA_prev, ((0, 0), (ph, ph),
+                                   (pw, pw), (0, 0)),
+                         mode='constant', constant_values=0)
 
     for i in range(m):
-        for j in range(h_new):
-            for k in range(w_new):
-                for len in range(c_new):
-                    vert_start = j * sh
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+
+                    vert_start = h * sh
                     vert_end = vert_start + kh
-                    horiz_start = k * sw
+                    horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
                     A_slice = A_prev_pad[i, vert_start:vert_end,
                                          horiz_start:horiz_end, :]
 
-                    dA_prev[i, vert_start:vert_end,
-                            horiz_start:horiz_end, :] += \
-                        W[:, :, :, ] * dZ[i, j, k, len]
-                    dW[:, :, :, len] += A_slice * dZ[i, j, k, len]
-                    db[:, :, :, len] += dZ[i, j, k, len]
+                    dA_prev_pad[i, vert_start:vert_end,
+                                horiz_start:horiz_end, :]\
+                        += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += A_slice * dZ[i, h, w, c]
 
     if padding == 'same':
-        dA_prev = dA_prev[:, ph:-ph, pw:-pw, :]
-
-    if activation == 'relu':
-        dA_prev = np.where(A_prev_pad > 0, dA_prev, 0)
+        dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+    else:
+        dA_prev = dA_prev_pad
 
     return dA_prev, dW, db
