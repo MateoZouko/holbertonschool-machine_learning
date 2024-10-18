b36cc9b941ffe3b2bdd804096ea52e562e47a91a
Mateo Zouko <mateozouko1@gmail.com>
2024-10-18 10:53:43 -0300

    wrongly uploaded main.py file

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -1,20 +1,84 @@
 #!/usr/bin/env python3
+"""
+Task 1
+"""
+from keras.models import load_model
+import numpy as np
+import math
 
-if __name__ == '__main__':
-    import numpy as np
-    Yolo = __import__('1-yolo').Yolo
 
-    np.random.seed(0)
-    anchors = np.array([[[116, 90], [156, 198], [373, 326]],
-                        [[30, 61], [62, 45], [59, 119]],
-                        [[10, 13], [16, 30], [33, 23]]])
-    yolo = Yolo('yolo.h5', 'coco_classes.txt', 0.6, 0.5, anchors)
-    output1 = np.random.randn(13, 13, 3, 85)
-    output2 = np.random.randn(26, 26, 3, 85)
-    output3 = np.random.randn(52, 52, 3, 85)
-    boxes, box_confidences, box_class_probs = yolo.process_outputs(
-        [output1, output2, output3], np.array([500, 700]))
+class Yolo():
+    """
+    Yolo class
+    """
 
-    print('Boxes:', boxes)
-    print('Box confidences:', box_confidences)
-    print('Box class probabilities:', box_class_probs)
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        self.model = load_model(model_path)
+        self.class_names = self.read_classes(classes_path)
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def read_classes(self, path):
+        """
+        Read the classes
+        """
+
+        list_class = []
+        file = open(path, mode='r')
+        classes = file.read().split("\n")
+        for i in classes[0:-1]:
+            list_class.append(i)
+
+        return list_class
+
+    def process_outputs(self, outputs, image_size):
+        """
+        Process the outputs
+        """
+        box_confidence = []
+        boxes = []
+        box_class_probs = []
+
+        for i, output in enumerate(outputs):
+
+            grid_height, grid_width, anchor_boxes, lastclasse = output.shape
+            image_height, image_width = image_size[0], image_size[1]
+
+            grid_x, grid_y = np.meshgrid(np.arange(grid_width),
+                                         np.arange(grid_height))
+            grid_x = grid_x.reshape(1, grid_height, grid_width, 1)
+            grid_y = grid_y.reshape(1, grid_height, grid_width, 1)
+
+
+            center_x = (1 / (1 + np.exp(-output[..., 0])) + grid_x)\
+                / grid_width * image_width
+
+            center_y = (1 / (1 + np.exp(-output[:, :, :, 1])) + grid_y)\
+                / grid_height * image_height
+
+            width = self.anchors[i][:, 0] * np.exp(output[:, :, :, 2])\
+                / self.model.input.shape[1] * image_width
+
+            height = self.anchors[i][:, 1] * np.exp(output[:, :, :, 3])\
+                / self.model.input.shape[2] * image_height
+
+            x1 = center_x - (width / 2)
+            y1 = center_y - (height / 2)
+            x2 = center_x + (width / 2)
+            y2 = center_y + (height / 2)
+
+            box = np.zeros((grid_height, grid_width, anchor_boxes, 4))
+            box[:, :, :, 0] = x1
+            box[:, :, :, 1] = y1
+            box[:, :, :, 2] = x2
+            box[:, :, :, 3] = y2
+            boxes.append(box)
+
+            confidence = 1 / (1 + np.exp(-output[:, :, :, 4]))
+            confidence = confidence.reshape(grid_height, grid_width,
+                                            anchor_boxes, 1)
+            box_confidence.append(confidence)
+            box_class_probs.append(1 / (1 + np.exp(-output[:, :, :, 5:])))
+
+        return boxes, box_confidence, box_class_probs
