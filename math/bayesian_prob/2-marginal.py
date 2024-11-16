c1b2e5f27b564c52338459653a1ced41917078df
Mateo Zouko <mateozouko1@gmail.com>
2024-11-16 18:18:49 -0300

    task 2

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -6,38 +6,51 @@ Task 2
 import numpy as np
 
 
-def posterior(x, n, P, Pr):
+def marginal(x, n, P, Pr):
     """
-    Calculates the posterior probability for the various hypothetical
-    probabilities of developing severe side effects given the data.
+    Calculates the marginal probability of obtaining the data.
+
+    Parameters:
+    - x: Number of patients that develop severe side effects (int)
+    - n: Total number of patients observed (int)
+    - P: 1D numpy.ndarray of hypothetical
+    probabilities (float values in [0, 1])
+    - Pr: 1D numpy.ndarray of prior beliefs
+    about P (float values in [0, 1])
+
+    Returns:
+    - The marginal probability of obtaining x and n
     """
+
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
+
     if type(x) is not int or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
+
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if np.any(P < 0) or np.any(P > 1):
+
+    if np.any((P < 0) | (P > 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
-    if np.any(Pr < 0) or np.any(Pr > 1):
+
+    if np.any((Pr < 0) | (Pr > 1)):
         raise ValueError("All values in Pr must be in the range [0, 1]")
+
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
 
-    fact = np.math.factorial(n) / (np.math.factorial(x)
-                                   * np.math.factorial(n - x))
+    fact = np.math.factorial(n) / (np.math.factorial(x) *
+                                   np.math.factorial(n - x))
     likelihood = fact * (P ** x) * ((1 - P) ** (n - x))
 
-    numerator = likelihood * Pr
+    marginal_probability = np.sum(likelihood * Pr)
 
-    evidence = np.sum(numerator)
-
-    posterior = numerator / evidence
-
-    output_values = posterior[posterior > 0][:3]
-    return output_values
+    return marginal_probability
