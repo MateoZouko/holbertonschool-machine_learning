3a9315f09a3dae3de08b33f4fd7eba0ae5a66422
Mateo Zouko <mateozouko1@gmail.com>
2024-11-17 20:25:00 -0300

    task 0

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,17 +8,11 @@ import numpy as np
 
 def pca(X, var=0.95):
     """
-    performs PCA on a dataset
+    Function that performs PCA on a dataset
     """
-
     U, s, Vt = np.linalg.svd(X)
-
     explained_variance = (s ** 2) / np.sum(s ** 2)
-
     cumulative_variance = np.cumsum(explained_variance)
-
     k = int(np.searchsorted(cumulative_variance, var) + 1)
-
     W = Vt[:k].T
-
     return W
