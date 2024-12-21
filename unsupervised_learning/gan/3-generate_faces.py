85878380d38ee7149b2c0d24a4553f74c5ae8448
Mateo Zouko <mateozouko1@gmail.com>
2024-12-21 15:05:06 +0800

    task 3 and 4

---
+++ unsupervised_learning/gan/3-generate_faces.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+Task 3
+"""
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+
+
+def convolutional_GenDiscr():
+    """
+    This function instantiates a generator and discriminator models for a GAN.
+    """
+    def generator():
+        """
+        This function returns a generator model.
+        """
+        input_layer = layers.Input(shape=(16,))
+
+        x = layers.Dense(2048)(input_layer)
+        x = layers.Reshape((2, 2, 512))(x)
+
+        x = layers.UpSampling2D()(x)
+        x = layers.Conv2D(64, (3, 3), padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('tanh')(x)
+
+        x = layers.UpSampling2D()(x)
+        x = layers.Conv2D(16, (3, 3), padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('tanh')(x)
+
+        x = layers.UpSampling2D()(x)
+        x = layers.Conv2D(1, (3, 3), padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        output_layer = layers.Activation('tanh')(x)
+
+        return Model(inputs=input_layer,
+                     outputs=output_layer, name="generator")
+
+    def get_discriminator():
+        """
+        This function returns a discriminator model.
+        """
+        input_layer = layers.Input(shape=(16, 16, 1))
+
+        x = layers.Conv2D(32, (3, 3), padding='same')(input_layer)
+        x = layers.MaxPooling2D()(x)
+        x = layers.Activation('tanh')(x)
+
+        x = layers.Conv2D(64, (3, 3), padding='same')(x)
+        x = layers.MaxPooling2D()(x)
+        x = layers.Activation('tanh')(x)
+
+        x = layers.Conv2D(128, (3, 3), padding='same')(x)
+        x = layers.MaxPooling2D()(x)
+        x = layers.Activation('tanh')(x)
+
+        x = layers.Conv2D(256, (3, 3), padding='same')(x)
+        x = layers.MaxPooling2D()(x)
+        x = layers.Activation('tanh')(x)
+
+        x = layers.Flatten()(x)
+        output_layer = layers.Dense(1)(x)
+
+        return Model(inputs=input_layer,
+                     outputs=output_layer, name="discriminator")
+
+    return generator(), get_discriminator()
