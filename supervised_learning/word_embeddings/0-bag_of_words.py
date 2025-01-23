f14d92c74d5e5de8c57a71813ada3663e1ac97eb
Mateo Zouko <mateozouko1@gmail.com>
2025-01-22 22:26:32 -0300

    task 0 and readme

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Task 0
+"""
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag of words embedding matrix
+    """
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X_train_counts = vectorizer.fit_transform(sentences)
+    embeddings = X_train_counts.toarray()
+    features = vectorizer.get_feature_names()
+    return embeddings, features
