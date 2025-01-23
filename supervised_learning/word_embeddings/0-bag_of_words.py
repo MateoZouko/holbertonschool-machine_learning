a4b6890e21631ef2302cb0658178f1fa15553c2c
Mateo Zouko <mateozouko1@gmail.com>
2025-01-22 22:28:57 -0300

    task 0

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -10,8 +10,9 @@ def bag_of_words(sentences, vocab=None):
     """
     Creates a bag of words embedding matrix
     """
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    X_train_counts = vectorizer.fit_transform(sentences)
-    embeddings = X_train_counts.toarray()
-    features = vectorizer.get_feature_names()
+    vector = CountVectorizer(vocabulary=vocab)
+    X = vector.fit_transform(sentences)
+    features = vector.get_feature_names()
+    embeddings = X.toarray()
+
     return embeddings, features
