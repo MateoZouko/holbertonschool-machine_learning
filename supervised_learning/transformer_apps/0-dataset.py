99c936fe4feacc91f596debc67d0015aeb01472d
Mateo Zouko <mateozouko1@gmail.com>
2025-02-02 15:55:49 -0300

    task 0

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -2,36 +2,54 @@
 """
 Task 0
 """
-import tensorflow.compat.v2 as tf
+
 import tensorflow_datasets as tfds
+import transformers
 
 
 class Dataset:
     """
-    Loads and preps a dataset
+    A class to load and prepare the TED HRLR translation dataset
+    for machine translation from Portuguese to English.
     """
 
     def __init__(self):
         """
-        Class constructor
+        Initializes the Dataset object and loads the training and validation
+        datasets.
         """
-        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
-                                       with_info=True,
-                                       as_supervised=True)
-        self.data_train, self.data_valid = (examples['train'],
-                                            examples['validation'])
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
 
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
 
     def tokenize_dataset(self, data):
         """
-        Creates sub-word tokenizers for our dataset
+        Tokenizes the dataset using pre-trained tokenizers and adapts them to
+        the dataset.
         """
-        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+        pt_sentences = []
+        en_sentences = []
+        for pt, en in data.as_numpy_iterator():
+            pt_sentences.append(pt.decode('utf-8'))
+            en_sentences.append(en.decode('utf-8'))
 
-        tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased', use_fast=True,
+            clean_up_tokenization_spaces=True)
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased', use_fast=True,
+            clean_up_tokenization_spaces=True)
 
-        return tokenizer_pt, tokenizer_en
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(pt_sentences,
+                                                            vocab_size=2**13)
+        tokenizer_en = tokenizer_en.train_new_from_iterator(en_sentences,
+                                                            vocab_size=2**13)
+
+        self.tokenizer_pt = tokenizer_pt
+        self.tokenizer_en = tokenizer_en
+
+        return self.tokenizer_pt, self.tokenizer_en
